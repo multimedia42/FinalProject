2e5d84713807141f20bdefbbd77c1510788f7e49
Yuetian <757356150@qq.com>
2019-04-19 21:51:27 +0200

    delete useless file

--- createTrackBar.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-void createTrackBar(String windowName, String trackBarName, int initialValue, int maxValue) {
-	int tb = createTrackbar(trackBarName, windowName, &initialValue, maxValue, 0, 0);
-}
