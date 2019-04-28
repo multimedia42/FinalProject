3b6c89f9234ec9bfc476314aea3a250e58c22425
linuslink57 <775458998@qq.com>
2019-04-28 20:55:10 +0200

    ok

--- Final.cpp
+++ Final.cpp
@@ -1,20 +1,23 @@
-﻿#include "Image.h"
-
+#include"Image.h"
+#include"Window.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
-
+enum ff{wwdd,ww};
 int main()
 {
-	Image *image = new Image("../Data/pano");
-
-	if (image->panorama() == EXIT_SUCCESS)
+	Window::function test = Window::function::resize;
+	Image* image = new Image("C:\\Users\\Administrator\\source\\repos\\multi\\x64\\Debug\\sample.jpg");
+	/*if (image->panorama() == EXIT_SUCCESS)
 	{
 		namedWindow("Pano", WINDOW_AUTOSIZE);
 		imshow("Pano", image->getDstMat());
-	}
-
+	}*/
+	Window window(*image, "test1");
+	window.create();
+	window.setTrackbar(test);
 	waitKey();
 	destroyAllWindows();
-
-	return 0;
 }
+
