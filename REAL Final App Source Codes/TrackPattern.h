603ddda8933a15b684df9565afc5438f931dae95
sajikdan <49645011+sajikdan@users.noreply.github.com>
2019-06-10 22:35:11 +0200

    REAL Final source codes

---
+++ REAL Final App Source Codes/TrackPattern.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include"Window.h"
+#include"Image.h"
+class TrackPattern
+{
+public:
+	
+private:
+	
+	void settrackbar(int function);
+};
+
